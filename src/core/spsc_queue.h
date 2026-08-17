3ed00c9eb7a3a832bfaffc3d792b6bd4c485bcb4
Dedeke David Iteoluwakiisi <thatsaxydev@gmail.com>
2026-08-17 09:53:54 +0100

    fix: correct condition in push method and ensure consistent use of unsigned integers

--- src/core/spsc_queue.h
+++ src/core/spsc_queue.h
@@ -27,13 +27,32 @@ namespace trdr::core
             const std::uint16_t tail = tail_.value.load(std::memory_order_relaxed);
             const std::uint16_t head = head_.value.load(std::memory_order_acquire);
 
-            if ((tail = head) == N) [[unlikely]]
+            if ((tail - head) == N) [[unlikely]]
             {
                 return false; // Queue is full
             }
             buffer_[tail & MASK] = value;
-            tail_.value.store(tail + 1, std::memory_order_release);
+            tail_.value.store(tail + 1U, std::memory_order_release);
             return true;
+        }
+
+        bool pop(T &value) noexcept
+        {
+            const std::uint16_t head = head_.value.load(std::memory_order_relaxed);
+            const std::uint16_t tail = tail_.value.load(std::memory_order_acquire);
+
+            if (head == tail) [[unlikely]]
+            {
+                return false; // Queue is empty
+            }
+            value = buffer_[head & MASK];
+            head_.value.store(head + 1U, std::memory_order_release);
+            return true;
+        }
+
+        [[nodiscard]] bool empty() const noexcept
+        {
+            return head_.value.load(std::memory_order_acquire) == tail_.value.load(std::memory_order_acquire);
         }
 
     private:
